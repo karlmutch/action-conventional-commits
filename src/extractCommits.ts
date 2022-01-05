5a28d83cfc49f3bf2a6cb33c7cd300a49c0c4dd9
Dan Hunt <dkhunt27@gmail.com>
2022-01-05 16:23:03 -0500

    added debugging

--- src/extractCommits.ts
+++ src/extractCommits.ts
@@ -7,15 +7,18 @@ type Commit = {
 };
 
 const extractCommits = async (context): Promise<Commit[]> => {
+    console.log({context})
     // For "push" events, commits can be found in the "context.payload.commits".
     const pushCommits = Array.isArray(get(context, "payload.commits"));
+    console.log({pushCommits})
     if (pushCommits) {
         return context.payload.commits;
     }
 
     // For PRs, we need to get a list of commits via the GH API:
-    const prCommitsUrl = get(context, "payload.pull_request.commits_url");
-    if (prCommitsUrl) {
+    const prNumber = get(context, "payload.pull_request.number");
+    console.log({prNumber})
+    if (prNumber) {
         try {
             const token = getInput('github-token')
             const github = getOctokit(token).rest
@@ -23,9 +26,11 @@ const extractCommits = async (context): Promise<Commit[]> => {
             const params = {
                 owner: context.repo.owner,
                 repo: context.repo.repo,
-                pull_number: context.payload.pull_request.number
+                pull_number: prNumber
             }
+            console.log({params})
             const data = await github.pulls.listCommits(params);
+            console.log({data})
 
             if (Array.isArray(data)) {
                 return data.map((item) => item.commit);
